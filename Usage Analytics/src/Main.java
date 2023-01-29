72de40e733edb80c60fa42391dd2d778720bf039
senya miroshnichenko <senya.miroshnicenko@icloud.com>
2023-01-30 00:32:27 +0300

    Analytics

---
+++ Usage Analytics/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        UsageAnalytics analytics = new UsageAnalytics();
+        analytics.countVisit();
+        analytics.countVisit();
+        int visits = analytics.countVisit();
+        System.out.println("Всего посещений: " + visits);
+
+
+    }
+}
